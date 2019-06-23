a866c9c495cb5c2e142a880b0c1e0a29c1c6d7c7
Shahwar Spall <shevy.spall@gmail.com>
2019-06-23 04:25:40 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -7,9 +7,9 @@ def introduction_language(name,language)
   puts "my name is #{name} and I am learning to program in #{language}."
 end
 
-introduction_language (Dan,Ember.js)
+introduction_language ("Dan","Ember.js")
 
 def introduction_with_language_optional (name,language)
   puts "my name is #{name} and I am learning to program in #{language}."
 end
-introduction_with_language_optional (Edwin, Python)
+introduction_with_language_optional ("Edwin", "Python")
